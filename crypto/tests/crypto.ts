a2ac09fde8aa84c370e644dd81ab9524f33e1021
Dnreikronos <dnreikronos2@gmail.com>
2025-10-26 16:18:16 -0300

    refactor: adjust deploy and crypto test file

--- crypto/tests/crypto.ts
+++ crypto/tests/crypto.ts
@@ -1,4 +1,16 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { Crypto } from "../target/types/crypto";
 
-describe("crypto", () => {});
+describe("crypto", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Crypto as Program<Crypto>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
